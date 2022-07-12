5e4e0ccb5fda282efd4c313e477140b56a5f7330
Do-it-chu <103571758+Do-it-chu@users.noreply.github.com>
2022-07-12 23:02:49 +0900

    🎉 Create Backend
    Create Backend

---
+++ backend/wil/src/main/java/com/wil/WilApplication.java
@@ -0,0 +1,13 @@
+package com.wil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WilApplication.class, args);
+	}
+
+}
