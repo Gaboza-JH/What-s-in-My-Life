61adb77a6c8e188bce1bb5777b1e73c3fedecbd9
Won jonghyeok <whdgur3077@hanmail.net>
2022-07-12 22:59:41 +0900

    🎉 create

---
+++ frontend/next-wil/components/user/Signup/Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Register from "../signup/signupInfo/Register";
+
+const Signup = () => {
+  return (
+    <>
+      <Register />
+    </>
+  );
+};
+
+export default Signup;
---
+++ frontend/next-wil/pages/user/login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Login from "../../components/user/Login/Login"
+
+const login = () => {
+  return (
+    <Login/>
+  )
+}
+
+export default login
