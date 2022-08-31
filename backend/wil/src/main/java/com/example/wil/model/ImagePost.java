bda20d87fe9f37ffa3d3173cf3d5a3351a505f9b
Do-it-chu <hot0721@naver.com>
2022-08-31 21:38:36 +0900

    image test

--- backend/wil/src/main/java/com/example/wil/model/ImagePost.java
+++ backend/wil/src/main/java/com/example/wil/model/ImagePost.java
@@ -15,7 +15,7 @@ public class ImagePost {
     private int img_id; // image 기본키값
 
     private String file_name; // image
-
+    private String file_url; // image를 가져올 수 있는 url주소
     @ManyToOne
     @JoinColumn(name="post_id")
     private Post post_id;
