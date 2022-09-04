947a65344b7086a106c1d3d10147471c7e0d2e8d
yewon <ajdanddl6321@gmail.com>
2022-09-05 00:46:11 +0900

    FEAT : Gallery Post content 삽입, Post Modal 적용

--- frontend/wil/src/Components/Senti/Senti.jsx
+++ frontend/wil/src/Components/Senti/Senti.jsx
@@ -1,36 +1,32 @@
-// import React from "react";
-// import styled from "styled-components"; // npm install styled-components
-// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-// import { Doughnut } from "react-chartjs-2"; // npm install react-chartjs-2 chart.js
-// import "./Senti.css";
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2"; // npm install react-chartjs-2 chart.js
+import "./Senti.css";
 
-// ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend);
 
-// const Senti = () => {
-//   const data = {
-//     labels: ["부정", "긍정"],
-//     datasets: [
-//       {
-//         label: "# of Votes",
-//         data: [3, 7],
-//         backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
-//         borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
-//         borderWidth: 1,
-//       },
-//     ],
-//   };
+const Senti = () => {
+    
+  const data = {
+    labels: ["부정", "긍정"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [3, 7],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
 
-//   return (
-//     <Container>
-//       <Doughnut data={data} />;
-//     </Container>
-//   );
-// };
+  return (
+    <div className="senti">
+      <Doughnut data={data} />;
+      <h1>감정 분석 결과</h1>
+    </div>
+  );
 
-// export default Senti;
+};
 
-// const Container = styled.div`
-//     width: 90vw;
-//     max-width: 300px; 
-//     // max-width로 크기 조절
-//   `;
+export default Senti;
