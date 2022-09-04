cc77604da28156452df4771bb9295d586b9beb2f
yewon <ajdanddl6321@gmail.com>
2022-09-04 17:05:41 +0900

    FEAT : Senti 컴포넌트 생성

--- frontend/wil/src/Components/Senti/Senti.jsx
+++ frontend/wil/src/Components/Senti/Senti.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components"; // npm install styled-components
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2"; // npm install react-chartjs-2 chart.js
+import "./Senti.css";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Senti = () => {
-  return (
-    <h1>감정 분석 결과 넣을 부분</h1>
-  )
-}
+  const data = {
+    labels: ["부정", "긍정"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [3, 7],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
 
-export default Senti
+  return (
+    <Container>
+      <Doughnut data={data} />;
+    </Container>
+  );
+};
+
+export default Senti;
+
+const Container = styled.div`
+    width: 90vw;
+    max-width: 300px; 
+    // max-width로 크기 조절
+  `;
