74bbad0717af27e53d79f95c7ba9849710d638a3
Won jonghyeok <whdgur3077@hanmail.net>
2022-09-13 23:57:12 +0900

    Fix : 로그인 아이콘 CSS 수정 및 불필요한 코드 삭제

--- frontend/wil/src/OAuth.jsx
+++
@@ -1,8 +0,0 @@
-export const OAUTH_REDIRECT_URL = "http://localhost:8080/login/oauth2/code/"
-
-export const NAVER_CLIENT_ID = "iOmtEYOzjWoW75Mq4FlE"
-
-// Naver
-export const NAVER_AUTH_URL = "https://nid.naver.com/oauth2.0/authorize?client_id=" + NAVER_CLIENT_ID
-                            + "&redirect_url=" + OAUTH_REDIRECT_URL + "naver"
-                            + "&response_type=token" + "&state=state"
