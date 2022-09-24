c9a43e4ca8d5e9c5fa45cf86464b666921833d3b
hwangjiwon98 <hjw9879@naver.com>
2022-09-24 23:39:26 +0900

    server test 4

--- frontend/wil/src/Components/S3/PostUpload.jsx
+++ frontend/wil/src/Components/S3/PostUpload.jsx
@@ -85,7 +85,7 @@ function PostUpload() {
       const modelRes = await axios.post(
         // `http://127.0.0.1:8080/predict`, textDTO,
         // `http://3.35.30.11:8000/predict`, textDTO,
-          `http://localhost:8080/predict/`, textDTO,
+          `http://3.37.184.148:8080/predict/`, textDTO,
         {
           withCredentials: true // 쿠키 cors 통신 설정
         },
