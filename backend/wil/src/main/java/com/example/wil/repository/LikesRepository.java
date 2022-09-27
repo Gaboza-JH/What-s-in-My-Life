f572093358b5fbc83c0107ab7bfb9edcf1744dc5
Do-it-chu <103571758+Do-it-chu@users.noreply.github.com>
2022-09-28 04:36:06 +0900

    Fix : test 27

--- backend/wil/src/main/java/com/example/wil/repository/LikesRepository.java
+++ backend/wil/src/main/java/com/example/wil/repository/LikesRepository.java
@@ -21,7 +21,7 @@ public interface LikesRepository extends JpaRepository<Likes, Integer> {
     int countByUserId(User user);
 
     @Query(value =
-            "SELECT ANY_VALUE(wil_db.c.like_id) as like_id, ANY_VALUE(wil_db.c.user_id) as user_id, ANY_VALUE(wil_db.c.post_id) as post_id" +
+            "SELECT ANY_VALUE(wil_db.c.like_id) as like_id, ANY_VALUE(wil_db.c.user_id) as user_id, ANY_VALUE(wil_db.c.post_id) as post_id " +
             "FROM likes AS c " +
             "GROUP BY c.post_id " +
             "ORDER BY count(wil_db.c.post_id) DESC " +
